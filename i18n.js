c72071014c0eb96359fecf939f7ba6c181ac8606
Zezombye <zezombye>
2020-12-11 13:46:44 +0100

    add i18n (except game end)

--- i18n.js
+++ i18n.js
@@ -1,86 +1,499 @@
 const translations = {
-    //Made by Zezombye | v1.0 | code: ABCDE
-    MADE_BY_ZEZ_1: {
-        "en-US": "Made by Zezombye | v",
-        "fr-FR": "Créé par Zezombye | v"
+
+    AUTO_GOLD_MINER: {
+        "en-US": "AUTO GOLD MINER",
+        "pt-BR": "Gerador de ouro",
     },
-    MADE_BY_ZEZ_2: {
-        "en-US": " | code: ",
-        "fr-FR": " | code : "
+    KILL_GOLD_BOUNTY: {
+        "en-US": "KILL GOLD BOUNTY",
+        "pt-BR": "Ouro por abate",
     },
-    //Do a kill with {0} Roll or {1} Piledriver to progress
-    HAMMOND_PROGRESS_1: {
-        "en-US": "Do a kill with ",
-        "fr-FR": "Faites un kill avec "
+    BASE_GOLD_STORAGE: {
+        "en-US": "BASE GOLD STORAGE",
+        "pt-BR": "Armazenamento da base",
     },
-    HAMMOND_PROGRESS_2: {
-        "en-US": " Roll or ",
-        "fr-FR": " Roulé-boulé ou "
+    MOVE_YOUR_BASE: {
+        "en-US": "MOVE YOUR BASE",
+        "pt-BR": "Mover base",
     },
-    HAMMOND_PROGRESS_3: {
-        "en-US": " Piledriver to progress",
-        "fr-FR": " Pilonnage pour progresser"
+    SWITCH_YOUR_HERO: {
+        "en-US": "SWITCH YOUR HERO",
+        "pt-BR": "Trocar de herói",
     },
-    RESPAWN_IN: {
-        "en-US": "Respawn in: ",
-        "fr-FR": "Réapparition dans : "
+    GOLD_STEAL_SPEED: {
+        "en-US": "GOLD STEAL SPEED",
+        "pt-BR": "Taxa de roubo de ouro",
     },
-    YOU_ARE_ON: {
-        "en-US": "You are on",
-        "fr-FR": "Vous êtes"
+    BASE_ENTRY_TIME: {
+        "en-US": "BASE ENTRY TIME",
+        "pt-BR": "Tempo de entrada na base",
     },
-    NEXT_HERO: {
-        "en-US": "Next hero:",
-        "fr-FR": "Prochain héros :"
+    AUTO_GOLD_MINER_DESCRIPTION: {
+        "en-US": "    [LVL {}]\n\n  Upgrades your gold generation\n    to {} per second. Can't generate\nwhen there are players in your base\n ",
+        "pt-BR": "    [NV {}]\n\n       MELHORA A GERAÇÃO DE OURO\nPARA {} POR SEGUNDO. NÃO FUNCIONA\n    QUANDO HÁ JOGADORES NA BASE.\n ",
+        format: "eventPlayer.statsUpgradeLevels[eventPlayer.upgradeCurrentSelection], eventPlayer.statsMoneySec + 1",
     },
-    FINAL_HERO: {
-        "en-US": "Final hero",
-        "fr-FR": "Dernier héros"
+    KILL_GOLD_BOUNTY_DESCRIPTION: {
+        "en-US": "     [LVL {}]\n\n    Increases bounty from {} to {}\nAwarded by dealing finishing blow\n  Doesn't affect dropped loot orbs\n ",
+        "pt-BR": "         [NV {}]\n\nAUMENTA A RECOMPENSA DE {} PARA {}\n          OBTIDO AO EFETUAR UM ABATE\n           NÃO AFETA ORBES DEIXADOS\n ",
+        format: "eventPlayer.statsUpgradeLevels[eventPlayer.upgradeCurrentSelection], eventPlayer.statsMoneyKill, eventPlayer.statsMoneyKill + 15",
     },
-    //Killed by {} Melee! You lost a level!
-    KILLED_BY_MELEE_1: {
-        "en-US": "Killed by ",
-        "fr-FR": "Tué par "
+    BASE_GOLD_STORAGE_DESCRIPTION: {
+        "en-US": "[LVL {0}]\n\nIncreases the maximum amount \n  of gold your base can store.\n  Upgraded storage: {1} gold\n ",
+        "pt-BR": "[NV {}]\n\n　　　　 Aumenta o máximo de ouro\n　　　 que a sua base pode armazenar.　　　 \n　　　　 Após melhoria: {} de ouro\n ",
+        format: "eventPlayer.statsUpgradeLevels[eventPlayer.upgradeCurrentSelection], ceil(eventPlayer.statsMoneyStorage * 1.5)",
     },
-    KILLED_BY_MELEE_2: {
-        "en-US": " Melee! You lost a level!",
-        "fr-FR": " Mêlée ! Vous perdez un niveau !"
+    MOVE_YOUR_BASE_DESCRIPTION: {
+        "en-US": "   [NO LVL]\n \n           Packs up your base. \nYou keep your gold & upgrades. \nPrice depends on gold storage.\n ",
+        "pt-BR": "　　　　[SEM NV]\n\n　　　　 Empacota a sua base.\n　 Ouro e melhorias são mantidos.\nO preço depende do armazenamento.\n ",
     },
-    //{} Melee Kill! You stole a level!
-    MELEE_KILL_1: {
+    SWITCH_YOUR_HERO_DESCRIPTION: {
+        "en-US": " [NO LVL]\n \n     Takes you to hero selection.\n  Bug: If only one hero shows up\nWait 5 seconds until they appear.\n ",
+        "pt-BR": "",
+    },
+    GOLD_STEAL_SPEED_DESCRIPTION: {
+        "en-US": "     [LVL {}]\n \n    Steal +2 gold/sec from bases.\nSame upgrade price as gold miner.\n  Total {} + enemy gold miner/sec.\n ",
+        "pt-BR": "[NV {}]\n\n　　　  Roube +2 ouro/seg de bases.\n  　 Mesmo preço do gerador de ouro.　   \n　　   Total {} + gerador inimigo/seg.\n ",
+        format: "eventPlayer.statsUpgradeLevels[eventPlayer.upgradeCurrentSelection], (eventPlayer.statsStealingSpeed + 1) * 2",
+    },
+    BASE_ENTRY_TIME_DESCRIPTION: {
+        "en-US": "  [LVL {}]\n \n      Reduces the time it takes\nto enter your base by 0.5 sec.\n    The current delay is {} sec.\n ",
+        "pt-BR": "[NV {}]\n\n　　　　     Reduz o tempo necessário\n  　　　para entrar na base por 0,5 seg.　　　  \n　　　　　   O tempo atual é {} seg.\n ",
+        format: "eventPlayer.statsUpgradeLevels[eventPlayer.upgradeCurrentSelection], eventPlayer.statsBaseDelay",
+    },
+    AUTO_GOLD_MINER_WHITESPACE: {
+        "en-US": "             ",
+        "pt-BR": "              ",
+    },
+    KILL_GOLD_BOUNTY_WHITESPACE: {
+        "en-US": "             ",
+        "pt-BR": "            ",
+    },
+    BASE_GOLD_STORAGE_WHITESPACE: {
+        "en-US": "               ",
+        "pt-BR": "                     ",
+    },
+    MOVE_YOUR_BASE_WHITESPACE: {
+        "en-US": "            ",
+        "pt-BR": "        ",
+    },
+    SWITCH_YOUR_HERO_WHITESPACE: {
+        "en-US": "              ",
+        "pt-BR": "            ",
+    },
+    GOLD_STEAL_SPEED_WHITESPACE: {
+        "en-US": "            ",
+        "pt-BR": "                      ",
+    },
+    BASE_ENTRY_TIME_WHITESPACE: {
+        "en-US": "           ",
+        "pt-BR": "             ",
+    },
+    NO_BASE_BUILT: {
+        "en-US": "{0} NO BASE BUILT {0}\n  PRESS [{1}] TO BUILD",
+        "pt-BR": "   {0} Base não construída {0}\nPressione {button} para construir",
+        format: "iconString(Icon.WARNING), buttonString(Button.INTERACT)",
+    },
+    ENEMY_IN_BASE: {
+        "en-US": "{0} ENEMY IN YOUR BASE! {0}",
+        "pt-BR": "{0} Inimigo na sua base! {0}",
+        format: "iconString(Icon.WARNING)",
+    },
+    WAITING_FOR_PLAYERS: {
+        "en-US": "Waiting for players to start ({}/{})",
+        "pt-BR": "Esperando jogadores para iniciar ({}/{})",
+        format: "getNumberOfPlayers(Team.ALL), NB_PLAYERS_AUTO_START",
+    },
+    STARTING_GAME: {
+        "en-US": "Starting the game...",
+        "pt-BR": "Iniciando jogo...",
+    },
+    MAP_NOT_SUPPORTED: {
+        "en-US": "The map {} IS NOT SUPPORTED!",
+        "pt-BR": "O mapa {} não é compatível!",
+        format: "getCurrentMap()",
+    },
+    BUILDING_NAVMESH: {
+        "en-US": "Building the navmesh, please wait",
+        "pt-BR": "Construindo a navmesh, aguarde",
+    },
+    GOLD_IN_BASE: {
+        "en-US": "GOLD IN BASE",
+        "pt-BR": "Ouro na base",
+    },
+    GOLD_PER_SEC: {
+        "en-US": "+{}/sec",
+        "pt-BR": "+{}/seg",
+        format: "eventPlayer.statsMoneySec",
+    },
+    GOLD_ON_PLAYER: {
+        "en-US": "GOLD ON PLAYER",
+        "pt-BR": "Ouro no jogador",
+    },
+    FULL: {
+        "en-US": "Full!",
+        "pt-BR": "Cheio!",
+    },
+    HOLD_TO_STEAL: {
+        "en-US": "HOLD {} TO STEAL {} GOLD: {}",
+        "pt-BR": "Segure {} para roubar {} ouro: {}",
+        format: "buttonString(Button.INTERACT), iconString(Icon.SKULL), eventPlayer.statsMoneyBase",
+    },
+    SCOREBOARD_TITLE: {
+        "en-US": "            discord.gg/YP544CH | code: "+GAMEMODE_CODE+"\nScoreboard    -    Base Wars "+VERSION+" "+CREATION_DATE,
+        "pt-BR": "        discord.gg/YP544CH | código: "+GAMEMODE_CODE+"\nPlacar    -    Guerra de Bases "+VERSION+" "+CREATION_DATE,
+    },
+    SCOREBOARD_TITLE_HIDDEN: {
+        "en-US": "Scoreboard    -    Base Wars "+VERSION+" "+CREATION_DATE,
+        "pt-BR": "Placar    -    Guerra de Bases "+VERSION+" "+CREATION_DATE,
+    },
+    SCOREBOARD_SUBTITLE: {
+        "en-US": "FIRST TO {0} GOLD IN BASE WINS!\n",
+        "pt-BR": "Vença ao obter {} de ouro na base!\n",
+        format: "gameGoldVictory",
+    },
+    XXX_GOLD: {
+        "en-US": " GOLD",
+        "pt-BR": " ouro",
+    },
+    ENTERING_BASE: {
+        "en-US": "{0} ENTERING BASE {0} \n           IN {1} SEC",
+        "pt-BR": "{0} Entrando na base {0} \n              em {1} seg",
+        format: "iconString(Icon.WARNING), eventPlayer.baseProtectionTimer",
+    },
+    PROTECTED_AND_INVISIBLE: {
+        "en-US": "         {0} PROTECTED AND INVISIBLE {0}\n\nTo upgrade, look at white center orb",
+        "pt-BR": "{0} PROTEGIDO(A) E INVISÍVEL {0}\n\n  PARA APRIMORAR, OLHE PARA\n       O ORBE BRANCO CENTRAL",
+        format: "iconString(Icon.CHECKMARK)",
+    },
+    CONTROLS: {
+        "en-US": "Controls:",
+        "pt-BR": "Controles:",
+    },
+    CONTROLS_CONTENT_1: {
+        "en-US": "{}{} : navigate menu\n[{}]    : buy upgrade",
+        "pt-BR": "{}{} : navegar pelo menu\n[{}]    : comprar melhoria",
+        format: "buttonString(Button.PRIMARY_FIRE), buttonString(Button.SECONDARY_FIRE), buttonString(Button.INTERACT)",
+    },
+    CONTROLS_CONTENT_2: {
+        "en-US": "[{}]   : use sparkbolt\n[{} {} remaining]",
+        "pt-BR": "[{}]   : usar faísca\n[{} {} restando]",
+        format: " buttonString(Button.ULTIMATE), eventPlayer.nbSparkbolts, iconString(Icon.BOLT)",
+    },
+    STEAL_RISK_BONUS: {
+        "en-US": "+{} GOLD ({} risk bonus)",
+        "pt-BR": "+{} de ouro ({} bônus de risco)",
+    },
+    STEAL_DISTANCE_BONUS: {
+        "en-US": "+{} GOLD ({} distance bonus)",
+        "pt-BR": "+{} de ouro ({} bônus de distância)",
+        format: "eventPlayer.tmpStealingAmount, eventPlayer.tmpStealingDistanceBonus",
+    },
+    STEAL_RISK_DISTANCE_BONUS: {
+        "en-US": "+{} GOLD ({} distance bonus, {} risk bonus)",
+        "pt-BR": "+{} de ouro ({} bônus de distância, {} bônus de risco)",
+        format: "eventPlayer.tmpStealingAmount, eventPlayer.tmpStealingDistanceBonus, eventPlayer.tmpStealingRiskBonus",
+    },
+    MORE_GOLD_SEC: {
+        "en-US": " +1 GOLD/SEC ",
+        "pt-BR": " +1 ouro/seg ",
+    },
+    MORE_KILL_BOUNTY: {
+        "en-US": " +15 KILL BOUNTY ",
+        "pt-BR": " +15 recompensa de abate ",
+    },
+    XXX_GOLD_KILL: {
+        "en-US": " GOLD (kill)",
+        "pt-BR": " ouro (abate)",
+    },
+    XXX_GOLD_CAPACITY: {
+        "en-US": " GOLD CAPACITY ",
+        "pt-BR": " capacidade de ouro ",
+    },
+    PACKED_UP_BASE: {
+        "en-US": " PACKED UP BASE ",
+        "pt-BR": " Base empacotada ",
+    },
+    MORE_GOLD_STEAL_SEC: {
+        "en-US": " +2 GOLD STEAL/SEC  ",
+        "pt-BR": " +2 ouro roubado/seg ",
+    },
+    LESS_ENTER_DELAY: {
+        "en-US": " -0.5 SEC DELAY ",
+        "pt-BR": " -0,5 seg de intervalo ",
+    },
+    TOO_FAR_AWAY: {
+        "en-US": "TOO FAR AWAY",
+        "pt-BR": "Muito longe",
+    },
+    AIM_AT_ENEMY: {
+        "en-US": "AIM AT ENEMY",
+        "pt-BR": "Mire no inimigo",
+    },
+    STORED_GOLD_IN_BASE: {
+        "en-US": "STORED {} GOLD IN BASE",
+        "pt-BR": "{} de ouro armazenado",
+        format: "eventPlayer.tmpMoneyDifference",
+    },
+    ENEMY_BASE_TOO_CLOSE: {
+        "en-US": " ENEMY BASE TOO CLOSE ",
+        "pt-BR": " Base inimiga muito próxima ",
+    },
+    BAD_BASE_LOCATION: {
+        "en-US": " BAD BASE LOCATION ",
+        "pt-BR": " Local de base inválido ",
+    },
+    NOT_ENOUGH_GOLD: {
+        "en-US": " NOT ENOUGH GOLD IN BASE ",
+        "pt-BR": " Ouro da base insuficiente ",
+    },
+    ANTI_CRASH_ACTIVATED: {
+        "en-US": "Anti-crash system activated",
+        "pt-BR": "Sistema anti-travamento ativado",
+    },
+    XXX_PLAYER_GOLD: {
+        "en-US": " PLAYER GOLD",
+        "pt-BR": " ouro do jogador",
+    },
+    XXX_SPARKBOLT: {
+        "en-US": " SPARKBOLT",
+        "pt-BR": " faísca",
+    },
+    XXXX: {
         "en-US": "",
-        "fr-FR": "Kill avec "
+        "pt-BR": "",
     },
-    MELEE_KILL_2: {
-        "en-US": " Melee Kill! You stole a level!",
-        "fr-FR": " Mêlée ! Vous volez un niveau!"
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
     },
-    //Do not start the game!\nHost ({}): please restart the game to play.
-    DONT_START_GAME_1: {
-        "en-US": "Do not start the game!\nHost (",
-        "fr-FR": "Ne commencez pas la partie !\nL'hôte ("
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
     },
-    DONT_START_GAME_2: {
-        "en-US": "): please restart the game to play.",
-        "fr-FR": ") doit redémarrer la partie pour jouer."
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
     },
-    //The map {} is not supported!\nSupported maps:
-    UNSUPPORTED_MAP_1: {
-        "en-US": "The map ",
-        "fr-FR": "La map "
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
     },
-    UNSUPPORTED_MAP_2: {
-        "en-US": " is not supported!\nSupported maps:",
-        "fr-FR": " n'est pas supportée !\nMaps supportées :"
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
     },
-    KILLED_YOURSELF: {
-        "en-US": "You killed yourself... You lost a level!",
-        "fr-FR": "Vous vous êtes suicidé... Vous perdez un niveau!"
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
     },
-    //{} wins!
-    PLAYER_WINS: {
-        "en-US": "wins!",
-        "fr-FR": "a gagné !"
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
+    },
+    XXXX: {
+        "en-US": "",
+        "pt-BR": "",
     },
     /*XXXXX: {
         "en-US": "",
--- todo.md
+++ todo.md
@@ -1,12 +1,12 @@
 
-- add oasis university (but prevent building a base on the road)
-
-- do not start the game immediately, but spawn 6 dummies when there are 6 players
-
-- fix loot not getting picked up - fixed ?
+- add oasis city center (but prevent building a base on the road)
 - fix loot orb not spawning - fixed?
-- delete "game not started" hud when game starts - fixed?
+- fix upgrade sometimes not appearing (you have to look away and back)
+- fix total gold earned calculation (seems to be 2x-3x reality)
 
+- do not start the game immediately, but spawn 6 dummies when there are 6 players - DONE
+- fix loot not getting picked up - DONE
+- delete "game not started" hud when game starts - DONE
 - fix base hud showing for other players - DONE
 - Fix "enemy in base" - DONE
 - Fix end game scoreboard - DONE
@@ -16,7 +16,6 @@
 - Add workshop settings - DONE
 - Add colors to leaderboard - DONE
 - fix leaderboard percentages - DONE
-- Fix hero select - fixed?
 
 Global effects: 16x loot orb
 Player effects (max 9):
